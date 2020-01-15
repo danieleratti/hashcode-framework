9bab7c7c5af6380a8498479aeb2e14ed1a69b3ae
Daniele Ratti <danieleratti1992@gmail.com>
2020-01-15 20:09:15 +0100

    2020-pizza input+reader

---
+++ rounds/2020-pizza/Reader.php
@@ -0,0 +1,22 @@
+<?php
+
+require __DIR__ . '/../../bootstrap.php';
+
+class Reader extends HashCode
+{
+    public $maxSlices; /* maximum number of slices to reach but not surpass */
+    public $pizzas; /* [pizzaId => numSlices] */
+
+    public function readInput()
+    {
+        list($maxSlices, $pizzas) = explode("\n", $this->inputContent);
+
+        list($maxSlices) = explode(" ", $maxSlices);
+        $this->maxSlices = (int)($maxSlices);
+
+        $pizzas = explode(" ", $pizzas);
+        foreach($pizzas as $id => $slices) {
+            $this->pizzas[] = (int)$slices;
+        }
+    }
+}
