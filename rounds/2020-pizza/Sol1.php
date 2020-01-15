222cf3245e7316f4c6b86f5775c63c5f0838d881
Daniele Ratti <danieleratti1992@gmail.com>
2020-01-15 20:19:21 +0100

    Update Sol1.php

--- rounds/2020-pizza/Sol1.php
+++ rounds/2020-pizza/Sol1.php
@@ -1,1 +1,20 @@
+<?php
+
+require __DIR__ . '/Reader.php';
+
+class Solution extends Reader
+{
     public $outputPizzas = [];
+
+    public function solve()
+    {
+
+        $this->output(count($this->outputPizzas) . "\n" . implode(" ", $this->outputPizzas)); //WARNING: in the order they are listed in the input
+    }
+}
+
+new Solution('a_example');
+//new Solution('b_small');
+//new Solution('c_medium');
+//new Solution('d_quite_big');
+//new Solution('e_also_big');
