a50c91a02ebb81f6b1bbfb87d3556b2bbc211726
Daniele Ratti <danieleratti1992@gmail.com>
2020-01-15 20:39:32 +0100

    Sol1 base

--- Utils.php
+++ Utils.php
@@ -1,6 +1,26 @@
 <?php
 
-class Utils
+function array_double_keysort(&$data, $_key, $sort=SORT_DESC, $_key2, $sort2=SORT_DESC)
 {
+    // Obtain a list of columns
+    foreach ($data as $key => $row) {
+        $sorter[$key]  = $row[$_key];
+        $sorter2[$key]  = $row[$_key2];
+    }
 
+    // Sort the data with volume descending, edition ascending
+    // Add $data as the last parameter, to sort by the common key
+    array_multisort($sorter, $sort, $sorter2, $sort2, $data);
+}
+
+function array_keysort(&$data, $_key, $sort=SORT_DESC)
+{
+    // Obtain a list of columns
+    foreach ($data as $key => $row) {
+        $sorter[$key]  = $row[$_key];
+    }
+
+    // Sort the data with volume descending, edition ascending
+    // Add $data as the last parameter, to sort by the common key
+    array_multisort($sorter, $sort, $data);
 }
--- rounds/2020-pizza/Sol1.php
+++ rounds/2020-pizza/Sol1.php
@@ -8,8 +8,19 @@ class Solution extends Reader
 
     public function solve()
     {
+        // algo
+        $this->outputPizzas[] = 1;
+        $this->outputPizzas[] = 2;
+        $this->outputPizzas[] = 0;
 
-        $this->output(count($this->outputPizzas) . "\n" . implode(" ", $this->outputPizzas)); //WARNING: in the order they are listed in the input
+        // output (don't touch below)
+        $score = 0;
+        foreach ($this->outputPizzas as $id) {
+            $score += $this->pizzas[$id];
+        }
+        asort($this->outputPizzas);
+        //$this->output(count($this->outputPizzas) . "\n" . implode(" ", array_values($this->outputPizzas))); //WARNING: in the order they are listed in the input
+        echo "Input: " . $this->inputName . " ***** Score: " . $score . " / " . $this->maxSlices . " (" . round($score/$this->maxSlices*100, 2) ."%)";
     }
 }
 
