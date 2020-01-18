c5b5b777a42fc28fb93b0e6230f6f46549a708d4
Silvio Deligios <silvio.deligios@gmail.com>
2020-01-18 14:21:25 +0100

    aggiunta utils di cache

---
+++ src/utils/Cache.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Utils;
+
+Class Cache
+{
+    protected $filename;
+    protected $dir;
+    public static $prefix;
+
+    public function __construct($filename)
+    {
+        $this->filename = $filename;
+        $this->dir = __DIR__ . '/../caches';
+    }
+
+    private function getFilename()
+    {
+        return $this->dir . '/' . (self::$prefix ? (self::$prefix . '_') : '') . $this->filename . '.json';
+    }
+
+    public function save($payload)
+    {
+        $fh = fopen($this->getFilename(), 'w');
+        fwrite($fh, json_encode($payload));
+        fclose($fh);
+    }
+
+    public function get()
+    {
+        $f = @file_get_contents($this->getFilename());
+        if (@$f)
+            return json_decode($f, true);
+        return false;
+    }
+}
