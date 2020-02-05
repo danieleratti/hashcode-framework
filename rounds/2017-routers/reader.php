a3ae01ccc7ebe7535c48a4b6012d7241bf4dbfdd
Silvio Deligios <silvio.deligios@gmail.com>
2020-02-05 20:46:14 +0100

    fix grid

--- rounds/2017-routers/reader.php
+++ rounds/2017-routers/reader.php
@@ -9,7 +9,11 @@ $contentRows = explode("\n", $fileManager->get());
 list($gridRows, $gridCols, $routerRange) = explode(' ', $contentRows[0]);
 list($backboneCosts, $routerCosts, $budget) = explode(' ', $contentRows[1]);
 list($backboneRow, $backboneCol) = explode(' ', $contentRows[2]);
-$grid = array_slice($contentRows, 3, count($contentRows));
+
+$grid = [];
+foreach (array_slice($contentRows, 3, count($contentRows)) as $stringRow) {
+    $grid[] = str_split($stringRow);
+}
 
 /**
  * @var integer $gridRows
