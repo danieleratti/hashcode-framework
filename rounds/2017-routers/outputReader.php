bf2363a2c7dc9594acf80e48c1b1f258fc2aa54a
Aronne Brivio <aroblu94@gmail.com>
2020-02-05 21:30:49 +0100

    Init outputReader

---
+++ rounds/2017-routers/outputReader.php
@@ -0,0 +1,80 @@
+<?php
+
+/**
+ * @var integer $gridRows
+ * @var integer $gridCols
+ * @var integer $routerRange
+ * @var integer $backboneCosts
+ * @var integer $routerCosts
+ * @var integer $budget
+ * @var integer $backboneRow
+ * @var integer $backboneCol
+ * @var integer[][] $gridArray
+ */
+
+$fileName = 'a';
+$outputName = 'a_example.txt';
+
+include 'reader.php';
+
+function isAdiacent($r1, $c1, $r2, $c2)
+{
+  return (abs($r1 - $r2) <= 1) && (abs($c1 - $c2) <= 1);
+}
+
+$content = trim(file_get_contents(__DIR__ . '/output/' . $outputName));
+$rows = explode("\n", $content);
+
+// Controlli
+
+$backboneLength = $row[0];
+array_shift($rows);
+$backbone = [];
+for ($i = 0; $i < $backboneLength; $i++) {
+  $backbone[] = explode(' ', $rows[0]);
+  array_shift($rows);
+}
+$routersNumber = $row[0];
+array_shift($rows);
+$routers = [];
+for ($i = 0; $i < $routersNumber; $i++) {
+  $routers[] = explode(' ', $rows[$i]);
+}
+for ($i = 0; $i < $backboneLength; $i++) {
+  $r = $backbones[$i][0];
+  $c = $backbones[$i][1];
+  if ($i >= 1) {
+    $r2 = $backbones[$i - 1][0];
+    $c2 = $backbones[$i - 1][1];
+  }
+  if (!isAdiacent($r, $c, $backboneRow, $backboneCol) && !($i >= 1 && isAdiacent($r, $c, $r2, $c2))) {
+    die("Backbone non valida");
+  }
+}
+for ($i = 0; $i < $routersNumber; $i++) {
+  $r = $routers[$i][0];
+  $c = $routers[$i][1];
+  $found = false;
+  for ($j = 0; $j < $backboneLength; $j++) {
+    $r2 = $backbones[$j][0];
+    $c2 = $backbones[$j][1];
+    if ($r == $r2 && $c == $c2) {
+      $found = true;
+      break;
+    }
+  }
+  if ($found) {
+    die("Il router non è connesso alla backbone");
+  }
+  if ($gridArray[$r][$c] == '#') {
+    die("Il router è sul muro");
+  }
+}
+$totalBackboneCost = $backboneLength * $backboneCosts;
+$totalRoutersCost = $routersNumber * $routerCosts;
+$total = $totalBackboneCost + $totalRoutersCost;
+if ($total > $budget) {
+  die("Il budget non è stato rispettato");
+}
+
+// Calcolo punteggio
--- rounds/2017-routers/reader.php
+++ rounds/2017-routers/reader.php
@@ -1,5 +1,17 @@
 <?php
 
+/**
+ * @var integer $gridRows
+ * @var integer $gridCols
+ * @var integer $routerRange
+ * @var integer $backboneCosts
+ * @var integer $routerCosts
+ * @var integer $budget
+ * @var integer $backboneRow
+ * @var integer $backboneCol
+ * @var integer[][] $gridArray
+ */
+
 use Utils\FileManager;
 
 require_once '../../bootstrap.php';
@@ -15,14 +27,24 @@ foreach (array_slice($contentRows, 3, count($contentRows)) as $stringRow) {
     $gridArray[] = str_split($stringRow);
 }
 
-/**
- * @var integer $gridRows
- * @var integer $gridCols
- * @var integer $routerRange
- * @var integer $backboneCosts
- * @var integer $routerCosts
- * @var integer $budget
- * @var integer $backboneRow
- * @var integer $backboneCol
- * @var integer[][] $gridArray
- */
+function isThereAWall($gridArray, $r1, $c1, $r2, $c2)
+{
+    for ($i = min($r1, $r2); $i < max($r1, $r2); $i++) {
+        for ($j = min($c1, $c2); $j < max($c1, $c2); $j++) {
+            if ($gridArray[$i][$j] == '#') {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+function addWifi($boolGrid, $gridArray, $r, $c, $wifiRange)
+{
+    for ($i = max(0, $r - $wifiRange); $i < min(count($gridArray), $r + $wifiRange); $i++) {
+        for ($j = max(0, $c - $wifiRange); $j < min(count($gridArray[$i]), $c + $wifiRange); $j++) {
+            $boolGrid[$i][$j] = !isThereAWall($gridArray, $i, $j, $r, $c);
+        }
+    }
+    return $boolGrid;
+}
