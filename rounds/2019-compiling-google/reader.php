9efc53872b3c68f039114ef0d38f581362faef0a
Silvio Deligios <silvio.deligios@gmail.com>
2020-02-12 19:34:15 +0100

    reader

---
+++ rounds/2019-compiling-google/reader.php
@@ -0,0 +1,45 @@
+<?php
+
+use Utils\FileManager;
+
+require_once '../../bootstrap.php';
+
+class File
+{
+    public $id;
+    public $compilingTime;
+    public $replicationTime;
+    public $dependenciesIds;
+
+    public $isTarget = false;
+    public $deadline;
+    public $goalPoints;
+
+    public function __construct($fileRow1, $fileRow2)
+    {
+        list($this->id, $this->compilingTime, $this->replicationTime) = explode(' ', $fileRow1);
+        $this->dependenciesIds = array_slice(explode(' ', $fileRow2), 1);
+    }
+}
+
+// Reading the inputs
+$fileManager = new FileManager('a');
+$fileRows = explode("\n", $fileManager->get());
+
+list($filesCount, $targetCount, $serversCount) = explode(' ', $fileRows[0]);
+$filesFileRows = array_slice($fileRows, 1, $filesCount * 2);
+$targetsFileRows = array_slice($fileRows, $filesCount * 2 + 1);
+
+/** @var File[] $files */
+$files = [];
+for ($i = 0; $i < count($filesFileRows); $i += 2) {
+    $file = new File($filesFileRows[$i], $filesFileRows[$i + 1]);
+    $files[$file->id] = $file;
+}
+
+foreach ($targetsFileRows as $targetsFileRow) {
+    list($id, $deadline, $points) = explode(' ', $targetsFileRow);
+    $files[$id]->isTarget = true;
+    $files[$id]->deadline = $deadline;
+    $files[$id]->goalPoints = $points;
+}
