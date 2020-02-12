5a8fef256e075eaba5c398174f18687531a36780
Silvio Deligios <silvio.deligios@gmail.com>
2020-02-12 20:08:15 +0100

    target file

---
+++ rounds/2019-compiling-google/stats.php
@@ -0,0 +1,28 @@
+<?php
+
+$fileName = 'b';
+
+require_once 'reader.php';
+
+$files = collect($files);
+echo "Numero di file: $filesCount\n";
+echo "Numero di target: $targetCount\n";
+echo "Numero di server: $serversCount\n\n";
+
+$max = $files->max('compilingTime');
+$min = $files->min('compilingTime');
+$avg = $files->avg('compilingTime');
+echo "Tempo compilazione: max $max min $min avg $avg\n\n";
+
+$max = $files->max('replicationTime');
+$min = $files->min('replicationTime');
+$avg = $files->avg('replicationTime');
+echo "Tempo replicazione: max $max min $min avg $avg\n\n";
+
+$targets =  collect($targets);
+echo "MAX SCORE: " . $targets->sum('goalPoints') . "\n";
+
+$max = $targets->max('deadline');
+$min = $targets->min('deadline');
+$avg = $targets->avg('deadline');
+echo "Deadline: max $max min $min avg $avg\n\n";
