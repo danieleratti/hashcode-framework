b8069505530bfc1c9548d20f0760783d90ab269e
Matteo Milesi <matteo.milesi@hotmail.it>
2020-02-13 01:25:43 +0100

    Scorer.

---
+++ rounds/2019-compiling-google/scorer.php
@@ -0,0 +1,10 @@
+<?php
+
+$fileName = 'f';
+
+require_once(__DIR__ . '/reader.php');
+
+/** @var TargetFile[] $targets */
+/** @var File[] $files */
+
+verifyOutput(file_get_contents('/Users/matteomilesi/git/hashcode/hashcodes/2019-final/output/14_45_39_f_big/output.txt'));
