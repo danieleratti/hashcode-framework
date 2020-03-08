f577c03d7794e870a4110c5f9253025090e77946
Daniele Ratti <danieleratti1992@gmail.com>
2020-03-08 15:31:39 +0100

    cerberus in sol1

--- rounds/2020-reply/sol1.php
+++ rounds/2020-reply/sol1.php
@@ -1,6 +1,11 @@
 <?php
 
-$fileName = 'a';
+use Utils\Cerberus;
+
+require_once '../../bootstrap.php';
+
+$fileName = 'd';
+Cerberus::runClient(['fileName' => $fileName]);
 
 include 'reader.php';
 
