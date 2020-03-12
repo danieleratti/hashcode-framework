761f7a883771319b17a1d136ff5f3de9443644a0
Daniele Ratti <danieleratti1992@gmail.com>
2020-03-12 17:17:39 +0100

    visual

--- rounds/2020-reply/analysis_visual.php
+++ rounds/2020-reply/analysis_visual.php
@@ -3,10 +3,18 @@
 use Utils\Visual\Colors;
 use Utils\Visual\VisualStandard;
 
-$fileName = 'a';
+$fileName = 'f';
 
 include 'reader.php';
 
-$visualStandard = new VisualStandard($rows, $cols);
-
-$visualStandard->save('visualEfficiency_' . $fileName);
+$visualStandard = new VisualStandard($WIDTH, $HEIGHT);
+foreach($tiles as $tile) {
+    /** @var Tile $tile */
+    if($tile->isDevDesk)
+        $visualStandard->setPixel($tile->r, $tile->c, Colors::blue5);
+    elseif($tile->isManagerDesk)
+        $visualStandard->setPixel($tile->r, $tile->c, Colors::red5);
+    else
+        $visualStandard->setPixel($tile->r, $tile->c, Colors::black);
+}
+$visualStandard->save('map_' . $fileName);
