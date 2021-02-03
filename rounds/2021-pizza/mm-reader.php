c48e3d1c64e239a51fe39ca88d37e0a96d781d07
Matteo Milesi <matteo.milesi@hotmail.it>
2021-02-03 02:42:21 +0100

    Sol.

--- rounds/2021-pizza/mm-reader.php
+++ rounds/2021-pizza/mm-reader.php
@@ -67,6 +67,34 @@ class PizzasManager
         $pizzas[$id] = $pizza;
         return $pizza;
     }
+
+    private static array $_uniqueCache = [];
+
+    /**
+     * @param Pizza[] $pizzas
+     * @return array
+     */
+    public static function uniqueIngredients(array $pizzas): array
+    {
+        $cacheKey = array_reduce($pizzas, function ($a, Pizza $p) {
+            return $a . '-' . $p->id;
+        }, '');
+        if (isset(static::$_uniqueCache[$cacheKey])) {
+            return static::$_uniqueCache[$cacheKey];
+        }
+
+        $unique = [];
+        foreach ($pizzas as $pizza) {
+            foreach ($pizza->ingredients as $ingredient) {
+                if (!isset($unique[$ingredient->name])) {
+                    $unique[$ingredient->name] = $ingredient;
+                }
+            }
+        }
+        static::$_uniqueCache[$cacheKey] = $unique;
+
+        return $unique;
+    }
 }
 
 class Pizza
@@ -141,6 +169,144 @@ class Team
     }
 }
 
+class CombinationsManager
+{
+    /**
+     * @var int[][][]
+     */
+    private static array $cacheCombinationsIdx = [];
+    /**
+     * @var Combination[]
+     */
+    private static array $cacheCombinations = [];
+
+    /**
+     * @param int[] $windowSizes
+     * @return void
+     */
+    public static function init(array $windowSizes): void
+    {
+        foreach ([2, 3, 4] as $k) {
+            /*$indexes = range(0, $windowSize - 1);
+            foreach (new Combinations($indexes, $k) as $c) {
+                static::$cacheCombinationsIdx[$k][] = $c;
+            }*/
+            $indexes = range(1, $windowSizes[$k - 2] - 1);
+            foreach (new Combinations($indexes, $k - 1) as $c) {
+                static::$cacheCombinationsIdx[$k][] = array_merge([0], $c);
+            }
+        }
+    }
+
+    /**
+     * @param Pizza[] $pizzas
+     * @param int $count
+     * @return Combination[]
+     */
+    public static function create(array $pizzas, int $count): array
+    {
+        $tempPizzas = array_values($pizzas);
+        $combinations = [];
+        foreach (static::$cacheCombinationsIdx[$count] as $cIdxs) {
+            //$cacheKey = implode(',', $cIdxs);
+            //if (isset(static::$cacheCombinations[$cacheKey])) {
+            //    $combination = static::$cacheCombinations[$cacheKey];
+            //} else {
+                $combinationPizzas = [];
+                foreach ($cIdxs as $cIdx) {
+                    if(!isset($tempPizzas[$cIdx]))
+                        continue 2;
+                    $combinationPizzas[] = $tempPizzas[$cIdx];
+                }
+                $combination = new Combination($combinationPizzas);
+                $combination->score = count(PizzasManager::uniqueIngredients($combinationPizzas));
+                //static::$cacheCombinations[$cacheKey] = $combination;
+            //}
+            $combinations[] = $combination;
+        }
+        return $combinations;
+    }
+}
+
+class Combination
+{
+    /** @var Pizza[] $pizzas */
+    public array $pizzas = [];
+    /** @var int $score */
+    public int $score = 0;
+
+    /**
+     * Combination constructor.
+     * @param array $pizzas
+     */
+    public function __construct(array $pizzas)
+    {
+        $this->pizzas = $pizzas;
+    }
+}
+
+class Combinations implements Iterator
+{
+    protected $c = null;
+    protected $s = null;
+    protected $n = 0;
+    protected $k = 0;
+    protected $pos = 0;
+
+    function __construct($s, $k)
+    {
+        $this->s = array_values($s);
+        $this->n = count($this->s);
+        $this->k = $k;
+        $this->rewind();
+    }
+
+    function key()
+    {
+        return $this->pos;
+    }
+
+    function current()
+    {
+        $r = [];
+        for ($i = 0; $i < $this->k; $i++)
+            $r[] = $this->s[$this->c[$i]];
+        return $r;
+    }
+
+    function next()
+    {
+        if ($this->_next())
+            $this->pos++;
+        else
+            $this->pos = -1;
+    }
+
+    function rewind()
+    {
+        $this->c = range(0, $this->k);
+        $this->pos = 0;
+    }
+
+    function valid()
+    {
+        return $this->pos >= 0;
+    }
+
+    protected function _next()
+    {
+        $i = $this->k - 1;
+        while ($i >= 0 && $this->c[$i] == $this->n - $this->k + $i)
+            $i--;
+        if ($i < 0)
+            return false;
+        $this->c[$i]++;
+        while ($i++ < $this->k - 1)
+            $this->c[$i] = $this->c[$i - 1] + 1;
+        return true;
+    }
+}
+
 // Reading the inputs
 $fileManager = new FileManager($fileName);
 $content = explode("\n", $fileManager->get());
@@ -170,4 +336,3 @@ for ($i = 0; $i < $T4; $i++) {
 
 unset($content);
 
-echo "a";
--- rounds/2021-pizza/mm-sol1.php
+++ rounds/2021-pizza/mm-sol1.php
@@ -2,12 +2,91 @@
 
 /** @noinspection PhpUndefinedVariableInspection */
 
-use Utils\Visual\Colors;
-use Utils\Visual\VisualStandard;
-use Utils\Analysis\Analyzer;
+$fileName = 'c';
 
-$fileName = 'a';
+/** @var Ingredient[] $ingredients */
+/** @var Pizza[] $pizzas */
+/** @var Team[] $teams */
+/** @var Team[][] $teamsBySize */
 
 require 'mm-reader.php';
 
+// Algo
 
+$windowSizes = [500, 20, 20];
+CombinationsManager::init($windowSizes);
+
+$totalScore = 0;
+$pizzasDelivered = 0;
+$availablePizzas = $pizzas;
+$windowPizzas = [];
+
+// Ordino le $availablePizzas
+uasort($availablePizzas, function (Pizza $p1, Pizza $p2) {
+    return count($p1->ingredients) < count($p2->ingredients);
+});
+
+foreach ([2, 3, 4] as $tc) {
+    // Prendo le prime $windowSize pizze
+    $availablePizzas = $windowPizzas + $availablePizzas;
+    $windowPizzas = array_slice($availablePizzas, 0, $windowSize, true);
+
+    foreach ($windowPizzas as $wp) {
+        unset($availablePizzas[$wp->id]);
+    }
+
+    foreach ($teamsBySize[$tc] as $team) {
+        echo $team->id ."\n";
+
+        // Calcolo tutte le combinazioni per la dimensione del team attuale e lo score
+        $combinations = CombinationsManager::create($windowPizzas, $tc);
+        $bestScore = -1;
+        $bestCombination = null;
+        foreach ($combinations as $c) {
+            if ($c->score > $bestScore) {
+                $bestScore = $c->score;
+                $bestCombination = $c;
+            }
+        }
+        if ($bestScore === -1) {
+            echo "Pizze finite (a).\n";
+            break 2;
+        }
+        $totalScore += $bestScore * $bestScore;
+        $pizzasDelivered += $tc;
+        $team->pizzas = $bestCombination->pizzas;
+
+        // Pulisco $windowPizzas
+        foreach ($team->pizzas as $pizza) {
+            unset($windowPizzas[$pizza->id]);
+        }
+        // Ripopolo $windowPizzas
+        $tpi = 0;
+        foreach ($availablePizzas as $id => $pizza) {
+            $windowPizzas[$id] = $pizza;
+            unset($availablePizzas[$id]);
+            $tpi++;
+            if($tpi >= $tc) break;
+        }
+        //array_push($windowPizzas, ...array_splice($availablePizzas, 0, $tc));
+        if (count($windowPizzas) < $tc) {
+            echo "Pizze finite (b).\n";
+            break 2;
+        }
+    }
+}
+
+echo "Score [$fileName]: $totalScore";
+
+// output
+$output = [];
+$output[] = count($teams);
+foreach ($teams as $team) {
+    $line = $team->size . "";
+    foreach ($team->pizzas as $pizza) {
+        $line .= ' ' . $pizza->id;
+    }
+    $output[] = $line;
+}
+
+$fileManager->output(implode("\n", $output), $totalScore . '__' . implode('_', $windowSizes));
