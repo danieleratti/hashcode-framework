682c2dc3804afd2ab70b374204e46994779dfe01
Stefano Zanini <stefanozanini@fattureincloud.it>
2021-02-08 21:35:58 +0100

    Create funzioni per estrarre un oggetto dalla collection con il valore min/max dell'attributo passato

--- utils/Collection.php
+++ utils/Collection.php
@@ -2,6 +2,8 @@
 
 namespace Utils;
 
+use Tightenco\Collect\Support\Arr;
+
 class Collection extends \Tightenco\Collect\Support\Collection
 {
     public function sortDesc()
@@ -11,5 +13,43 @@ class Collection extends \Tightenco\Collect\Support\Collection
                 return 0;
             return ($a > $b) ? -1 : 1;
         });
+    }
+
+    /**
+     * Get and remove the item with the maximum value of the given key.
+     *
+     * @param string $key
+     * @return mixed
+     */
+    public function pullMax($key) {
+        $maxIndex = null;
+        $maxValue = null;
+        foreach ($this->items as $index => $item) {
+            if($maxValue === null || $item->{$key} > $maxValue) {
+                $maxIndex = $index;
+                $maxValue = $item->{$key};
+            }
+        }
+
+        return Arr::pull($this->items, $maxIndex);
+    }
+
+    /**
+     * Get and remove the item with the minimum value of the given key.
+     *
+     * @param string $key
+     * @return mixed
+     */
+    public function pullMin($key) {
+        $minIndex = null;
+        $minValue = null;
+        foreach ($this->items as $index => $item) {
+            if($minValue === null || $item->{$key} < $minValue) {
+                $minIndex = $index;
+                $minValue = $item->{$key};
+            }
+        }
+
+        return Arr::pull($this->items, $minIndex);
     }
 }
