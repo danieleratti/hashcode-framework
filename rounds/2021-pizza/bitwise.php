216372c1ee88441467126f6c4d1fdb4c66a88651
Daniele Ratti <danieleratti1992@gmail.com>
2021-02-09 19:35:47 +0100

    bozza bitwise

--- rounds/2021-pizza/bitwise.php
+++ rounds/2021-pizza/bitwise.php
@@ -7,6 +7,16 @@ $dec2 = bindec("111111");
 //printf("%'06b", 0b110101 & 0b011001); // 010001
 //printf("%'32b", $dec1 & $dec2);
 
+/*
+$t = microtime(true);
+for($i=0;$i<1000*1000*1000;$i++) {
+	$x = ($dec1 & $dec2);
+}
+$t = microtime(true)-$t;
+echo "T=$t";
+die();
+*/
+
 $cmp = str_pad(decbin($dec1 & $dec2), 6, "0", STR_PAD_RIGHT);
 
 echo "CMP = $cmp";
