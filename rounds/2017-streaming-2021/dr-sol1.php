f600fb9ed7a5bfb15f90032bced5b2754e95291f
Daniele Ratti <danieleratti1992@gmail.com>
2021-02-16 19:15:55 +0100

    variables

--- rounds/2017-streaming-2021/dr-sol1.php
+++ rounds/2017-streaming-2021/dr-sol1.php
@@ -7,6 +7,14 @@ $fileName = 'a';
 
 include 'dr-reader.php';
 
-/** @var Collection|Foo[] $foo */
+/** @var int $N_VIDEOS */
+/** @var int $N_ENDPOINTS */
+/** @var int $N_REQUESTS */
+/** @var int $N_CACHES */
+/** @var int $SIZE_CACHES */
+/** @var Collection|Video[] $VIDEOS */
+/** @var Collection|Endpoint[] $ENDPOINTS */
+/** @var Collection|Request[] $ENDPOINTS */
+/** @var Collection|Cache[] $CACHES */
 
 Log::out("Fine");
