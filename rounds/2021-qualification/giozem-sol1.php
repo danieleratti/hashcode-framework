7d5290ecd70604e2d019e96a4db041a236f04fbf
Giorgio Bertolotti <giorgiobertolotti@fattureincloud.it>
2021-02-25 18:22:30 +0100

    :tada: Init sol giozem

---
+++ rounds/2021-qualification/giozem-sol1.php
@@ -0,0 +1,36 @@
+<?php
+
+use Utils\Autoupload;
+use Utils\Cerberus;
+use Utils\Collection;
+use Utils\FileManager;
+use Utils\Log;
+
+require_once '../../bootstrap.php';
+
+/* CONFIG */
+$fileName = null;
+$param1 = null;
+Cerberus::runClient(['fileName' => 'a' /*, 'param1' => 1.0*/]);
+Autoupload::init();
+
+include 'dr-reader.php';
+
+
+/* VARIABLES */
+/** @var FileManager $fileManager */
+/** @var Collection|Foo[] $foo */
+
+$SCORE = 0;
+
+
+/* ALGO */
+Log::out("Run with fileName $fileName");
+$SCORE = $param1;
+
+
+/* OUTPUT */
+Log::out('Output...');
+$output = "xxx";
+$fileManager->outputV2($output, 'score_' . $SCORE);
+//Autoupload::submission($fileName, null, $output);
