a5ec01d6f2d1f1b2a3c7497926bff7ff50bb0c2d
Giorgio Bertolotti <giorgiobertolotti@fattureincloud.it>
2021-02-25 19:30:49 +0100

    :sparkles: Add path duration to car

--- rounds/2021-qualification/giozem-reader.php
+++ rounds/2021-qualification/giozem-reader.php
@@ -107,11 +107,11 @@ $N_STREETS = (int)$N_STREETS;
 $N_INTERSECTIONS = (int)$N_INTERSECTIONS;
 $DURATION = (int)$DURATION;
 $streetIdxStart = 1;
-$streetIdxEnd = $streetIdxStart + $N_STREETS - 1;
-$carsIdxStart = $streetIdxEnd;
-$carsIdxEnd = $carsIdxStart + $N_CARS - 1;
+$streetIdxEnd = $streetIdxStart + $N_STREETS;
+$carsIdxStart = $streetIdxEnd + 1;
+$carsIdxEnd = $carsIdxStart + $N_CARS;
 
-for ($i = 0; $i < $N_INTERSECTIONS; $i++)
+for($i=0;$i<$N_INTERSECTIONS;$i++)
     $INTERSECTIONS[$i] = new Intersection();
 
 for ($streetIdx = $streetIdxStart; $streetIdx <= $streetIdxEnd; $streetIdx++) {
@@ -122,18 +122,12 @@ for ($streetIdx = $streetIdxStart; $streetIdx <= $streetIdxEnd; $streetIdx++) {
 for ($carsIdx = $carsIdxStart; $carsIdx <= $carsIdxEnd; $carsIdx++) {
     $c = explode(" ", $content[$carsIdx]);
     $streets = [];
-    foreach ($c as $k => $v) {
-        if ($k > 0) {
+    foreach($c as $k => $v) {
+        if($k > 0) {
             $streets[] = $STREETS[$v];
         }
     }
     $CARS[] = new Car($streets);
-}
-
-foreach($STREETS as $street) {
-    /** @var Street $street */
-    $street->start->streetsOut[] = $street;
-    $street->end->streetsIn[] = $street;
 }
 
 Log::out("Read finished");
--- rounds/2021-qualification/giozem-sol1.php
+++ rounds/2021-qualification/giozem-sol1.php
@@ -19,7 +19,14 @@ include 'dr-reader.php';
 
 /* VARIABLES */
 /** @var FileManager $fileManager */
-/** @var Collection|Foo[] $foo */
+/** @var Collection|Street[] $STREETS */
+/** @var Collection|Intersection[] $INTERSECTIONS */
+/** @var Collection|Car[] $CARS */
+/** @var int $DURATION */
+/** @var int $N_INTERSECTIONS */
+/** @var int $N_STREETS */
+/** @var int $N_CARS */
+/** @var int $BONUS */
 
 $SCORE = 0;
 
@@ -32,5 +39,5 @@ $SCORE = $param1;
 /* OUTPUT */
 Log::out('Output...');
 $output = "xxx";
-$fileManager->outputV2($output, 'score_' . $SCORE);
+//$fileManager->outputV2($output, 'score_' . $SCORE);
 //Autoupload::submission($fileName, null, $output);
