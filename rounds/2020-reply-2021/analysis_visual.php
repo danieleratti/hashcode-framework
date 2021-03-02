f3b66f870129feb453afa55dd7ac6cdf1136adbf
Matteo Milesi <matteo.milesi@hotmail.it>
2021-03-02 20:34:36 +0100

    Reader fix.

--- rounds/2020-reply-2021/analysis_visual.php
+++ rounds/2020-reply-2021/analysis_visual.php
@@ -3,10 +3,19 @@
 use Utils\Visual\Colors;
 use Utils\Visual\VisualStandard;
 
-$fileName = 'a';
+$fileName = 'f';
 
 include __DIR__ . '/reader.php';
 
-$visualStandard = new VisualStandard($rows, $cols);
+$visualStandard = new VisualStandard($rowsCount, $columnsCount);
+$visualStandard->setBg(Colors::white);
 
-$visualStandard->save('visualEfficiency_' . $fileName);
+for ($row = 0; $row < $rowsCount; $row++) {
+    for ($col = 0; $col < $columnsCount; $col++) {
+        if ($MAP[$row][$col]) {
+            $visualStandard->setPixel($row, $col, $MAP[$row][$col]->type === 'M' ? Colors::red5 : Colors::blue5);
+        }
+    }
+}
+
+$visualStandard->save('standard_' . $fileName);
