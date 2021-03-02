5c9b21ef402995ea332331843a7ccdd1f408b129
Matteo Milesi <matteo.milesi@hotmail.it>
2021-03-02 18:21:41 +0100

    MM

--- rounds/2021-qualification/mm-classes.php
+++ rounds/2021-qualification/mm-classes.php
@@ -16,28 +16,17 @@ class Street
     /** @var Intersection $end */
     public $end;
 
-    // Global attrs
-    /** @var Semaphore $semaphore */
-    public $semaphore;
-    /** @var int $usage */
-    public $usage;
-
     public function __construct($name, $duration, $start, $end)
     {
         $this->name = $name;
         $this->duration = (int)$duration;
         $this->start = $start;
         $this->end = $end;
-        $this->semaphore = new Semaphore($this);
-        $this->usage = 0;
-
-        $this->end->streetsIn[$this->name] = $this;
-        $this->start->streetsOut[$this->name] = $this;
+        $this->init();
     }
 
     public function init()
     {
-        $this->semaphore->init();
     }
 }
 
@@ -58,37 +47,18 @@ class Semaphore
     // History attrs
     /** int $maxQueue */
     public $maxQueue;
-    /** int $queueTime */
-    public $queueTime;
-    /** int $totalQueue */
-    public $totalQueue;
-
-    /** @var array $history */
-    public $history;
 
     public function __construct(Street $street)
     {
         $this->street = $street;
         $this->intersection = $street->end;
+        $this->init();
     }
 
     public function init()
     {
-        $this->queue = [];
-        $this->timeDuration = 1;
-        $this->queueTime = 0;
-        $this->maxQueue = 0;
-        $this->totalQueue = 0;
-    }
-
-    public function saveHistory()
-    {
-        $this->history[] = [
-            'timeDuration' => $this->timeDuration,
-            'queueTime' => $this->queueTime,
-            'maxQueue' => $this->maxQueue,
-            'totalQueue' => $this->totalQueue,
-        ];
+        $this->semaphoreTime = [];
+        $this->greenScheduling = [];
     }
 
     public function getLoad()
@@ -98,20 +68,10 @@ class Semaphore
         }, 0);
     }
 
-    public function update()
-    {
-        $currentQueue = count($this->queue);
-        if ($currentQueue > 0)
-            $this->queueTime = 0;
-        if ($currentQueue > $this->maxQueue)
-            $this->maxQueue = $currentQueue;
-        $this->totalQueue += $currentQueue;
-    }
-
     public function enqueueCar(Car $car)
     {
         array_unshift($this->queue, $car);
-        //$this->maxQueue = max(count($this->queue), $this->maxQueue);
+        $this->maxQueue = max(count($this->queue), $this->maxQueue);
     }
 
 }
@@ -125,59 +85,37 @@ class Intersection
     /** @var Street[] $streetsOut */
     public $streetsOut = [];
 
-    // Global attrs
-    /** @var Car[] $passingCars */
-    public $passingCars;
-
     // History attrs
     /** @var Street[] $greenScheduling */
     public $greenScheduling = [];
-    /** @var Car[] $remainingCars */
-    public $remainingCars = [];
 
     public function __construct($id)
     {
         $this->id = $id;
-        $this->passingCars = [];
+        $this->init();
     }
 
     public function init()
     {
         $this->greenScheduling = [];
-        $this->remainingCars = $this->passingCars;
     }
 
-    public function calculateTimeDuration()
+    public function addStreetIn(Street $streetIn, $semaphoreTime = 1)
     {
-        global $DURATION;
-        $scores = [];
-        $worstScore = 10000000000;
-        foreach ($this->streetsIn as $streetName => $street) {
-            $sem = $street->semaphore;
-            $score = $sem->totalQueue / $DURATION + pow($sem->maxQueue, 0.5) + $sem->queueTime / $DURATION;
-            $scores[$streetName] = $score;
-            if($score < $worstScore) {
-                $worstScore = $score;
-            }
-        }
-        foreach ($this->streetsIn as $streetName => $street) {
-            $sem = $street->semaphore;
-            $sem->timeDuration = round($scores[$streetName] / $worstScore);
-        }
+        $this->streetsIn[$streetIn->name] = $streetIn;
+        $this->semaphoreTime[$streetIn->name] = $semaphoreTime;
     }
 
-    public function calculateScheduling()
+    public function addStreetOut(Street $streetOut)
     {
-        // Se ho solo una strada entrante
-        if (count($this->streetsIn) === 1) {
-            $this->greenScheduling = [
-                array_values($this->streetsIn)[0],
-            ];
-            return;
-        }
+        $this->streetsOut[$streetOut->name] = $streetOut;
+    }
+
+    public function updateScheduling()
+    {
         $this->greenScheduling = [];
-        foreach ($this->streetsIn as $streetName => $street) {
-            for ($i = 0; $i < $street->semaphore->timeDuration; $i++) {
+        foreach ($this->semaphoreTime as $streetName => $st) {
+            for ($i = 0; $i < $st; $i++) {
                 $this->greenScheduling[] = $this->streetsIn[$streetName];
             }
         }
@@ -185,30 +123,21 @@ class Intersection
 
     public function nextStep(int $t)
     {
-        if (!$this->greenScheduling) {
-            Log::error('Non c\'è uno scheduling');
-        }
+        if (!$this->greenScheduling) return;
         $currentStreet = $this->greenScheduling[$t % count($this->greenScheduling)];
         if ($currentStreet) {
-            if (count($currentStreet->semaphore->queue) > 0) {
-                $car = array_pop($currentStreet->semaphore->queue);
+            if ($currentStreet->queue) {
+                $car = array_pop($currentStreet->queue);
                 $car->nextStreet();
-                unset($this->remainingCars[$car->id]);
-                return true;
             }
         } else {
-            Log::error('Non c\'è una strada verde');
+            Log::out('Non c\'è una strada verde');
         }
-        return false;
     }
 }
 
 class Car
 {
-    const STATE_ALREADY_ENQUEUED = 0;
-    const STATE_JUST_ENQUEUED = 1;
-    const STATE_TRAVELING = 2;
-
     private static $lastId = 0;
 
     // Base attrs
@@ -233,21 +162,20 @@ class Car
     /** @var int currentStreetDuration */
     public $currentStreetDuration = 0;
     /** @var int currentStreetEnqueued */
-    public $isEnqueued = false;
+    public $currentStreetEnqueued = false;
 
     public function __construct($streets)
     {
         $this->id = self::$lastId++;
         $this->streets = $streets;
+        $this->init();
 
         $this->pathDuration = 0;
         foreach ($streets as $street) {
             /** @var Street $street */
-            if ($street !== $this->streets[0]) {
+            if ($street !== $this->startingStreet) {
                 $this->pathDuration += $street->duration;
-                $street->start->passingCars[$this->id] = $this;
             }
-            $street->usage++;
         }
     }
 
@@ -260,22 +188,24 @@ class Car
 
     public function nextStep()
     {
-        if ($this->currentStreetDuration > 1) {
+        if ($this->currentStreetDuration > 0) {
             $this->currentStreetDuration--;
-            return self::STATE_TRAVELING;
         } else {
-            if (!$this->isEnqueued) {
-                $this->enqueue();
-                return self::STATE_JUST_ENQUEUED;
+            if (!$this->currentStreetEnqueued) {
+                return $this->enqueue();
             }
         }
-        return self::STATE_ALREADY_ENQUEUED;
+        return false;
     }
 
     public function enqueue()
     {
-        $this->currentStreet->semaphore->enqueueCar($this);
-        $this->isEnqueued = true;
+        if ($this->currentStreetIdx == count($this->streets) - 1) { //era l'ultima strada
+            return true;
+        }
+        $this->currentStreet->enqueueCar($this);
+        $this->currentStreetEnqueued = true;
+        return false;
     }
 
     public function nextStreet()
@@ -283,6 +213,6 @@ class Car
         $this->currentStreetIdx++;
         $this->currentStreet = $this->streets[$this->currentStreetIdx];
         $this->currentStreetDuration = $this->currentStreet->duration;
-        $this->isEnqueued = false;
+        $this->currentStreetEnqueued = false;
     }
 }
--- rounds/2021-qualification/mm-reader.php
+++ rounds/2021-qualification/mm-reader.php
@@ -5,189 +5,12 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ERROR);
 
 require_once '../../bootstrap.php';
+require_once './mm-classes.php';
 
 use Utils\FileManager;
 use Utils\Log;
 
 $fileName = @$fileName ?: 'a';
-
-// Classes
-class Street
-{
-    private static $lastId = 0;
-    /** @var int $id */
-    //public $id;
-    /** @var string $name */
-    public $name;
-    /** @var int $duration */
-    public $duration;
-    /** @var Intersection $start */
-    public $start;
-    /** @var Intersection $end */
-    public $end;
-    /** int $usage */
-    public $usage = 0;
-    /** @var Car[] $queue */
-    public $queue = [];
-
-    public function __construct($name, $duration, $start, $end)
-    {
-        //$this->id = self::$lastId++;
-        $this->name = $name;
-        $this->duration = (int)$duration;
-        $this->start = $start;
-        $this->end = $end;
-    }
-
-    public function getPriority()
-    {
-        // TODO: ritorna la somma delle priority delle auto in queue che hanno finito la strada (ovvero che sono in coda al semaforo)
-        $priority = array_reduce($this->queue, function ($carry, Car $car) {
-            return $carry + $car->priority;
-        }, 0);
-        return $priority;
-    }
-
-    public function enqueueCar(Car $car)
-    {
-        array_unshift($this->queue, $car);
-    }
-}
-
-class Intersection
-{
-    private static $lastId = 0;
-    /** @var int $id */
-    public $id;
-    /** @var Street[] $streetsIn */
-    public $streetsIn = [];
-    /** @var Street[] $streetsOut */
-    public $streetsOut = [];
-
-    /** @var bool $fixedGreen */
-    public $fixedGreen = false;
-    /** @var int[] $semaphoreTime */
-    public $semaphoreTime = [];
-    /** @var Street[] $greenScheduling */
-    public $greenScheduling = [];
-
-    public function __construct()
-    {
-        $this->id = self::$lastId++;
-    }
-
-    public function addStreetIn(Street $streetIn)
-    {
-        $this->streetsIn[$streetIn->name] = $streetIn;
-        $this->semaphoreTime[$streetIn->name] = 1;
-    }
-
-    public function addStreetOut(Street $streetOut)
-    {
-        $this->streetsOut[$streetOut->name] = $streetOut;
-    }
-
-    public function updateScheduling()
-    {
-        $this->greenScheduling = [];
-        foreach ($this->semaphoreTime as $streetName => $st) {
-            for ($i = 0; $i < $st; $i++) {
-                $this->greenScheduling[] = $this->streetsIn[$streetName];
-            }
-        }
-    }
-
-    public function nextStep(int $t)
-    {
-        $currentStreet = $this->greenScheduling[$t % count($this->greenScheduling)];
-        if ($currentStreet) {
-            $car = array_pop($currentStreet->queue);
-            $car->nextStreet();
-        } else {
-            Log::out('Non c\'è una strada verde');
-        }
-    }
-}
-
-class Car
-{
-    private static $lastId = 0;
-    /** @var int $id */
-    public $id;
-    /** @var Street[] $streets */
-    public $streets;
-    /** @var Street $startingStreet */
-    public $startingStreet;
-    /** @var Street $currentStreet */
-    //public $currentStreet;
-    /** @var int $pathDuration */
-    public $pathDuration;
-    /** @var int $nStreets */
-    public $nStreets;
-    /** @var int $priority */
-    public $priority;
-
-    /** @var Street $currentStreet */
-    public $currentStreet;
-    /** @var int $currentStreetIdx */
-    public $currentStreetIdx = 1;
-    /** @var int currentStreetDuration */
-    public $currentStreetDuration = 0;
-    /** @var int currentStreetEnqueued */
-    public $currentStreetEnqueued = false;
-
-    public function __construct($streets)
-    {
-        $this->id = self::$lastId++;
-        $this->streets = $streets;
-        $this->startingStreet = $streets[0];
-        $this->currentStreet = $streets[0];
-
-        $this->pathDuration = 0;
-        $isFirst = true;
-        foreach ($streets as $street) {
-            /** @var Street $street */
-            if ($isFirst) {
-                // Ignore the first street because the car is already at the street end
-                $isFirst = false;
-                continue;
-            }
-            $this->pathDuration += $street->duration;
-            $street->usage++;
-        }
-        $this->nStreets = count($streets);
-    }
-
-    public function nextStep()
-    {
-        if ($this->currentStreetDuration > 0) {
-            $this->currentStreetDuration--;
-        } else {
-            if (!$this->currentStreetEnqueued) {
-                return $this->enqueue();
-            }
-        }
-        return false;
-    }
-
-    public function enqueue()
-    {
-        if ($this->currentStreetIdx == count($this->streets) - 1) { //era l'ultima strada
-            return true;
-        }
-        $this->currentStreet->enqueueCar($this);
-        $this->currentStreetEnqueued = true;
-        return false;
-    }
-
-    public function nextStreet()
-    {
-        $this->currentStreetIdx++;
-        $this->currentStreet = $this->streets[$this->currentStreetIdx];
-        $this->currentStreetDuration = $this->currentStreet->duration;
-        $this->currentStreetEnqueued = false;
-    }
-}
 
 // Variables
 $DURATION = 0;
@@ -216,7 +39,7 @@ $carsIdxStart = $streetIdxEnd + 1;
 $carsIdxEnd = $carsIdxStart + $N_CARS - 1;
 
 for ($i = 0; $i < $N_INTERSECTIONS; $i++)
-    $INTERSECTIONS[$i] = new Intersection();
+    $INTERSECTIONS[$i] = new Intersection($i);
 
 for ($streetIdx = $streetIdxStart; $streetIdx <= $streetIdxEnd; $streetIdx++) {
     [$start, $end, $name, $duration] = explode(" ", $content[$streetIdx]);
@@ -232,12 +55,6 @@ for ($carsIdx = $carsIdxStart; $carsIdx <= $carsIdxEnd; $carsIdx++) {
         }
     }
     $CARS[] = new Car($streets);
-}
-
-foreach ($STREETS as $streetName => $street) {
-    /** @var Street $street */
-    $street->start->addStreetOut($street);
-    $street->end->addStreetIn($street);
 }
 
 Log::out("Read finished");
