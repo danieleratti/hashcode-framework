3e18644fc6e04f6af3e9baa2aa6f69c11c057b70
Sara Locatelli <saralocatelli@fattureincloud.it>
2021-03-02 21:03:12 +0100

    output writer

--- rounds/2021-qualification-rnd/sol1.php
+++ rounds/2021-qualification-rnd/sol1.php
@@ -1,7 +1,35 @@
 <?php
 
+use Utils\FileManager;
+use Utils\Log;
+
 $fileName = 'a';
 
 include 'reader.php';
+
+/** @var ProjectManager[] $PROJECTMANAGERS */
+/** @var Developer[] $DEVELOPERS */
+/** @var FileManager $fileManager */
+
+Log::out('Output...');
+foreach ($DEVELOPERS as $k=>$dev){
+    $y= $dev->posH;
+    $x= $dev->posW;
+    if($y && $x)
+        $output .= $x .' '.$y. PHP_EOL;
+    else
+        $output .= 'X' . PHP_EOL;
+}
+foreach ($PROJECTMANAGERS as $pm){
+    $y= $pm->posH;
+    $x= $pm->posW;
+    if($y && $x)
+        $output .= $x .' '.$y. PHP_EOL;
+    else
+        $output .= 'X' . PHP_EOL;
+}
+$fileManager->outputV2($output, time());
+
+
 
 die();
