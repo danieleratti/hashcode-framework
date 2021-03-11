18bdfdccbfab28364aa56b06b552220a3c5bbafc
Sebastian Senderowski <sebastiansenderowski@fattureincloud.it>
2021-03-11 16:29:16 +0100

    Aggiuntta solseb1

---
+++ rounds/2021-reply-so-che-a-voi-piace/sol-seb1.php
@@ -0,0 +1,42 @@
+<?php
+
+use Utils\Autoupload;
+use Utils\Cerberus;
+use Utils\Collection;
+use Utils\FileManager;
+use Utils\Log;
+
+require_once '../../bootstrap.php';
+
+/* CONFIG */
+$fileName = 'a';
+Cerberus::runClient(['fileName' => $fileName]);
+Autoupload::init();
+include 'reader.php';
+
+/* VARIABLES */
+/** @var FileManager $fileManager */
+/** @var Collection|Foo[] $FOO */
+/** @var int $DURATION */
+
+$SCORE = 0;
+
+/* FUNCTIONS */
+/**
+ * @param $semaphores
+ * @return string
+ */
+function getOutput($semaphores)
+{
+    $output = [];
+    $output[] = count($semaphores);
+    $output = implode("\n", $output);
+    return $output;
+}
+
+/* ALGO */
+
+
+/* SCORING & OUTPUT */
+Log::out("SCORE($fileName) = ");
+//$fileManager->outputV2(getOutput([]), 'score_' . $SCORE);
