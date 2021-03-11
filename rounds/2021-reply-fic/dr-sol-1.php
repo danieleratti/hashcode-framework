4cf56d5b69dc173f4006899e563286e14d90dcc6
Daniele Ratti <danieleratti1992@gmail.com>
2021-03-11 17:31:54 +0100

    getNearBuildings

--- rounds/2021-reply-fic/dr-sol-1.php
+++ rounds/2021-reply-fic/dr-sol-1.php
@@ -21,7 +21,11 @@ include __DIR__ . '/dr-reader.php';
 /** @var Building[] $BUILDINGS */
 /** @var Antenna[] $ANTENNAS */
 
+define('bigPixelSize', 100); // TUNE THIS
+
 $SCORE = 0;
+$bigPixel2antennas = [];
+$bigPixel2buildings = [];
 
 /* FUNCTIONS */
 /**
@@ -36,7 +40,60 @@ function getOutput($semaphores)
     return $output;
 }
 
+function dist($r1, $c1, $r2, $c2)
+{
+    return abs($r1 - $r2) + abs($c1 - $c2);
+}
+
+function getBigPixel($r, $c)
+{
+    return [floor($r / bigPixelSize), floor($c / bigPixelSize)];
+}
+
+function getNearBuildings($r, $c, $range)
+{
+    global $bigPixel2buildings, $BUILDINGS;
+
+    //if ($range == -1)
+    //    return $BUILDINGS;
+
+    //if (!$bigPixelNeighbors)
+    //    $bigPixelNeighbors = bigPixelNeighbors;
+
+    $bigPixelNeighbors = ceil($range / bigPixelSize);
+    $bigPixel = getBigPixel($r, $c);
+    $r = $bigPixel[0];
+    $c = $bigPixel[1];
+
+    $buildings = [];
+    for ($_r = $r - $bigPixelNeighbors; $_r <= $r + $bigPixelNeighbors; $_r++) {
+        for ($_c = $c - $bigPixelNeighbors; $_c <= $c + $bigPixelNeighbors; $_c++) {
+            if ($bigPixel2buildings[$_r][$_c]) {
+                foreach($bigPixel2buildings[$_r][$_c] as $building) {
+                    $dist = dist($r, $c, $building->r, $building->c);
+                    if($dist <= $range)
+                        $buildings[] = ['building' => $building, 'distance' => $dist];
+                }
+                //$buildings = array_merge($buildings, $bigPixel2buildings[$_r][$_c]);
+            }
+        }
+    }
+
+    return $buildings;
+}
+
+
 /* ALGO */
+Log::out("Heating bigPixels...");
+foreach ($BUILDINGS as $building) {
+    $bigPixel = getBigPixel($building->r, $bigPixel2buildings->c);
+    $bigPixel2buildings[$bigPixel[0]][$bigPixel[1]][$building->id] = $building;
+}
+
+$buildings = getNearBuildings(0, 0, 7);
+die();
+
+// megacells
 
 
 /* SCORING & OUTPUT */
