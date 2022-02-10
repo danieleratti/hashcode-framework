e6e7fe97a7752fcdabf23da3cb436ca00a8c5e2b
Matteo Milesi <matteo.milesi@hotmail.it>
2022-02-10 21:16:18 +0100

    Prima di gigio.

--- rounds/2022-02-10-pizza/sol.php
+++ rounds/2022-02-10-pizza/sol.php
@@ -11,7 +11,7 @@ global $clients;
 /** @var Ingredient[] */
 global $ingredients;
 
-$fileName = 'a';
+$fileName = 'e';
 
 include_once 'reader.php';
 //include_once 'analyzer.php';
@@ -20,14 +20,29 @@ $output = [];
 
 // Codice
 
-$sortedIngredients = $ingredients;
+$sortedIngredients = array_values($ingredients);
 usort($sortedIngredients, fn(Ingredient $i1, Ingredient $i2) => count($i1->dislikedBy) < count($i2->dislikedBy));
+
+$i = 0;
+while (count($sortedIngredients) > 0) {
+    /** @var Ingredient $current */
+    $current = $sortedIngredients[$i];
+    if(count($current->dislikedBy) > 0) {
+        unset($sortedIngredients[$i]);
+    } else {
+        break;
+    }
+    $i++;
+}
 
 foreach ($sortedIngredients as $i) {
     echo $i . PHP_EOL;
 }
 
+echo "\n\n";
+echo getScoreByIngredients($sortedIngredients);
+
 // Output
-$output = count($output) . ' ' . implode(' ', $output);
-Log::out('Output...');
+$output = count($sortedIngredients) . ' ' . implode(' ', $output);
+//Log::out('Output...');
 $fileManager->outputV2($output);
