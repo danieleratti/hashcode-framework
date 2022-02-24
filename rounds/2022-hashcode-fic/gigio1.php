60be8ceac421b14ee569a4a9c452a4abbac2def1
Silvio Deligios <silvio.deligios@gmail.com>
2022-02-24 19:59:57 +0100

    algo scemo

--- rounds/2022-hashcode-fic/gigio1.php
+++ rounds/2022-hashcode-fic/gigio1.php
@@ -1,6 +1,42 @@
 <?php
 
-$fileName = 'a';
+$fileName = 'f';
+
+/** @var Contributor[] */
+global $contributors;
+
+/** @var Project[] */
+global $projects;
+
+/** @var Output */
+global $OUTPUT;
 
 /* Reader */
 include_once 'reader.php';
+
+/** @var Project $project */
+foreach ($projects as $project) {
+    $selectedContributors = [];
+    foreach ($project->roles as $role) {
+        $selected = null;
+        /** @var Contributor $contributor */
+        foreach ($contributors as $contributor) {
+            if ($contributor->skills[$role['skill']] >= $role['level'] && !isset($selectedContributors[$contributor->name])) {
+                $selected = $contributor;
+                break;
+            }
+        }
+
+        if (!$selected)
+            break;
+
+        $selectedContributors[$selected->name] = $selected;
+    }
+
+    if (count($selectedContributors) < count($project->roles))
+        continue;
+
+    $OUTPUT->setProject($project, $selectedContributors);
+}
+
+$OUTPUT->save();
