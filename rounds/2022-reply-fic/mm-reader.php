39075bcee667b5b0959d22c00c1dab5cd00b994d
Matteo Milesi <matteo.milesi@hotmail.it>
2022-03-10 17:00:51 +0100

    Reader

---
+++ rounds/2022-reply-fic/mm-reader.php
@@ -0,0 +1,65 @@
+<?php
+
+global $fileName;
+
+/** @var Demon[] */
+global $demons;
+
+/** @var int */
+global $initialStamina;
+/** @var int */
+global $maxStamina;
+/** @var int */
+global $turnsNumber;
+/** @var int */
+global $demonsCount;
+
+use Utils\FileManager;
+
+require_once '../../bootstrap.php';
+
+class Demon
+{
+    /** @var int */
+    public int $id;
+    /** @var int */
+    public int $staminaRequired;
+    /** @var int */
+    public int $staminaRecoverTime;
+    /** @var int */
+    public int $staminaRecoverAmount;
+    /** @var int */
+    public int $fragmentsRewardTime;
+    /** @var int[] */
+    public array $fragmentsRewardDetails = [];
+}
+
+/* Reading the input */
+$fileManager = new FileManager($fileName);
+$content = explode("\n", $fileManager->get());
+
+$fileRow = 0;
+
+list($initialStamina, $maxStamina, $turnsNumber, $demonsCount) = explode(' ', $content[$fileRow++]);
+$initialStamina = (int)$initialStamina;
+$maxStamina = (int)$maxStamina;
+$turnsNumber = (int)$turnsNumber;
+$demonsCount = (int)$demonsCount;
+
+$demons = [];
+
+for ($i = 0; $i < $demonsCount; $i++) {
+    list($requiredStamina, $staminaRecoverTime, $staminaRecoverAmount, $fragmentsRewardTime, $fragmentsDetails) = explode(' ', $content[$fileRow++], 5);
+    $d = new Demon();
+    $d->id = $i;
+    $d->staminaRequired = (int)$requiredStamina;
+    $d->staminaRecoverTime = (int)$staminaRecoverTime;
+    $d->staminaRecoverAmount = (int)$staminaRecoverAmount;
+    $d->fragmentsRewardTime = (int)$fragmentsRewardTime;
+    $fragmentsDetails = explode(' ', $fragmentsDetails);
+    foreach ($fragmentsDetails as $k => &$f) {
+        $f = (int)$f;
+    }
+    $d->fragmentsRewardDetails = $fragmentsDetails;
+    $demons[$i] = $d;
+}
