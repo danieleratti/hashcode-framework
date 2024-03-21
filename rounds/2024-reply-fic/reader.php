d04fd6f2cf19679e49efbfc6d7f070b70e2916cc
Matteo Milesi <matteo@fattureincloud.it>
2024-03-21 17:33:11 +0100

    Visualizer

--- rounds/2024-reply-fic/reader.php
+++ rounds/2024-reply-fic/reader.php
@@ -3,31 +3,32 @@
 global $fileName;
 
 use Utils\FileManager;
+use Utils\Visual\VisualGradient;
 
 require_once '../../bootstrap.php';
 
 class GoldenPoint
 {
-    public int $x;
-    public int $y;
+    public int $r;
+    public int $c;
 
-    public function __construct(int $x, int $y)
+    public function __construct(int $r, int $c)
     {
-        $this->x = $x;
-        $this->y = $y;
+        $this->r = $r;
+        $this->c = $c;
     }
 }
 
 class SilverPoint
 {
-    public int $x;
-    public int $y;
+    public int $r;
+    public int $c;
     public int $score;
 
-    public function __construct(int $x, int $y, int $score)
+    public function __construct(int $r, int $c, int $score)
     {
-        $this->x = $x;
-        $this->y = $y;
+        $this->r = $r;
+        $this->c = $c;
         $this->score = $score;
     }
 }
@@ -46,11 +47,67 @@ class TileType
     }
 }
 
+class MapManager
+{
+    public string $filename;
+    public array $map;
+    public int $rowsCount;
+    public int $columnsCount;
+
+    public function __construct(string $filename, int $rowsCount, int $columnsCount)
+    {
+        $this->filename = $filename;
+        $this->rowsCount = $rowsCount;
+        $this->columnsCount = $columnsCount;
+        $this->map = array_fill(0, $rowsCount, array_fill(0, $columnsCount, null));
+    }
+
+    public function setGoldenPoint(GoldenPoint $point): void
+    {
+        $this->map[$point->r][$point->c] = $point;
+    }
+
+    public function setSilverPoint(SilverPoint $point): void
+    {
+        $this->map[$point->r][$point->c] = $point;
+    }
+
+    public function visualize(): void
+    {
+        $visualGradient = new VisualGradient($this->rowsCount, $this->columnsCount);
+
+        $maxAbsScore = 0;
+
+        for ($c = 0; $c < $this->columnsCount; $c++) {
+            for ($r = 0; $r < $this->rowsCount; $r++) {
+                $v = $this->map[$r][$c];
+                if ($v instanceof SilverPoint) {
+                    $maxAbsScore = max($maxAbsScore, $v->score);
+                }
+            }
+        }
+
+        for ($c = 0; $c < $this->columnsCount; $c++) {
+            for ($r = 0; $r < $this->rowsCount; $r++) {
+                $v = $this->map[$r][$c];
+                if ($v instanceof GoldenPoint) {
+                    $color = [0xff, 0x00, 0x00];
+                } elseif ($v instanceof SilverPoint) {
+                    $color = [0x00, 0xff - ($v->score / $maxAbsScore * 0x7f), 0x00];
+                } else {
+                    $color = [0x00, 0x00, 0x00];
+                }
+                $visualGradient->setCustomPixel($r, $c, ...$color);
+            }
+        }
+
+        $visualGradient->save($this->filename);
+    }
+}
+
 /* Reading the input */
 $fileManager = new FileManager($fileName);
-$content = explode("\n", $fileManager->get());
-
-[$W, $H, $Gn, $Sm, $Tl] = explode(' ', $content[0]);
+$content = explode("\r\n", $fileManager->get());
 $fileRow = 0;
 
 /** @var int $W */
@@ -70,10 +127,12 @@ $silverPoints = [];
 $tiles = [];
 
 for ($i = 0; $i < $Gn; $i++) {
-    $goldenPoints[$i] = new GoldenPoint(...explode(' ', $content[$fileRow++]));
+    [$c, $r] = explode(' ', $content[$fileRow++]);
+    $goldenPoints[$i] = new GoldenPoint($r, $c);
 }
 for ($i = 0; $i < $Sm; $i++) {
-    $silverPoints[$i] = new SilverPoint(...explode(' ', $content[$fileRow++]));
+    [$c, $r, $score] = explode(' ', $content[$fileRow++]);
+    $silverPoints[$i] = new SilverPoint($r, $c, $score);
 }
 for ($i = 0; $i < $Tl; $i++) {
     $tiles[$i] = new TileType(...explode(' ', $content[$fileRow++]));
@@ -82,3 +141,12 @@ for ($i = 0; $i < $Tl; $i++) {
 print_r($goldenPoints);
 print_r($silverPoints);
 print_r($tiles);
+
+$mapManager = new MapManager($fileName, $H, $W);
+foreach ($goldenPoints as $gp) {
+    $mapManager->setGoldenPoint($gp);
+}
+foreach ($silverPoints as $sp) {
+    $mapManager->setSilverPoint($sp);
+}
+$mapManager->visualize();
