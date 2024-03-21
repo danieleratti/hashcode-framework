8d1abd69ee8a80713d4b2bbf9502881c1e69d5d2
Matteo Milesi <matteo@fattureincloud.it>
2024-03-21 16:46:23 +0100

    Inputs and reader

---
+++ rounds/2024-reply-fic/mm-sol-1.php
@@ -0,0 +1,44 @@
+<?php
+
+use Utils\ArrayUtils;
+use Utils\Autoupload;
+use Utils\Cerberus;
+use Utils\FileManager;
+use Utils\Log;
+
+require_once __DIR__ . '/../../bootstrap.php';
+
+global $fileName;
+/** @var FileManager */
+global $fileManager;
+
+/* Config & Pre runtime */
+$fileName = 'a';
+//$param1 = 1;
+//Cerberus::runClient(['fileName' => $fileName, 'param1' => $param1]);
+
+/* Reader */
+include_once 'reader.php';
+
+/* Classes */
+
+/* Functions */
+function getOutput()
+{
+    global $snakes;
+    ArrayUtils::array_keysort_objects($snakes, 'id', SORT_ASC);
+    $output = [];
+    return implode("\n", $output);
+}
+
+
+/* Vars */
+/** @var Snake[] $snakes */
+/** @var int $snakesCount */
+
+
+// RUN
+$SCORE = 0;
+Log::out("Run started...");
+
+#$fileManager->outputV2(getOutput(), $SCORE);
--- rounds/2024-reply-fic/reader.php
+++ rounds/2024-reply-fic/reader.php
@@ -6,16 +6,79 @@ use Utils\FileManager;
 
 require_once '../../bootstrap.php';
 
-class Demo
+class GoldenPoint
 {
-    public string $name;
+    public int $x;
+    public int $y;
+
+    public function __construct(int $x, int $y)
+    {
+        $this->x = $x;
+        $this->y = $y;
+    }
+}
+
+class SilverPoint
+{
+    public int $x;
+    public int $y;
+    public int $score;
+
+    public function __construct(int $x, int $y, int $score)
+    {
+        $this->x = $x;
+        $this->y = $y;
+        $this->score = $score;
+    }
+}
+
+class TileType
+{
+    public string $id;
+    public int $cost;
+    public int $count;
+
+    public function __construct(string $id, int $cost, int $count)
+    {
+        $this->id = $id;
+        $this->cost = $cost;
+        $this->count = $count;
+    }
 }
 
 /* Reading the input */
 $fileManager = new FileManager($fileName);
 $content = explode("\n", $fileManager->get());
 
+[$W, $H, $Gn, $Sm, $Tl] = explode(' ', $content[0]);
 $fileRow = 0;
 
+/** @var int $W */
+/** @var int $H */
+/** @var int $Gn */
+/** @var int $Sm */
+/** @var int $Tl */
+[$W, $H, $Gn, $Sm, $Tl] = explode(' ', $content[$fileRow++]);
+$W = (int)trim($W);
+$H = (int)trim($H);
+$Gn = (int)trim($Gn);
+$Sm = (int)trim($Sm);
+$Tl = (int)trim($Tl);
 
-print_r($content);
+$goldenPoints = [];
+$silverPoints = [];
+$tiles = [];
+
+for ($i = 0; $i < $Gn; $i++) {
+    $goldenPoints[$i] = new GoldenPoint(...explode(' ', $content[$fileRow++]));
+}
+for ($i = 0; $i < $Sm; $i++) {
+    $silverPoints[$i] = new SilverPoint(...explode(' ', $content[$fileRow++]));
+}
+for ($i = 0; $i < $Tl; $i++) {
+    $tiles[$i] = new TileType(...explode(' ', $content[$fileRow++]));
+}
+
+print_r($goldenPoints);
+print_r($silverPoints);
+print_r($tiles);
